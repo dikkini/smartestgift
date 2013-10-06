177064563df249c340ec0d77882b494ea9aadb9c
dikkini <858683@gmail.com>
2013-10-06 16:05:05 +0400

    great refactoring

--- src/main/java/com/smartestgift/hibernate/HibernateUtil.java
+++ src/main/java/com/smartestgift/hibernate/HibernateUtil.java
@@ -13,8 +13,7 @@ public class HibernateUtil {
             // Create the SessionFactory from hibernate.cfg.xml
             return new AnnotationConfiguration().configure().buildSessionFactory();
 
-        }
-        catch (Throwable ex) {
+        } catch (Throwable ex) {
             // Make sure you log the exception, as it might be swallowed
             System.err.println("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
---
+++ src/main/java/com/smartestgift/service/model/Gift.java
@@ -0,0 +1,11 @@
+package com.smartestgift.service.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dikkini
+ * Date: 10/6/13
+ * Time: 2:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Gift {
+}
