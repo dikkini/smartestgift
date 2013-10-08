0117de4fd09593b550f51d237e6af6fed8434cdc
dikkini <858683@gmail.com>
2013-10-08 16:40:47 +0400

    transaction model

--- src/main/java/com/smartestgift/service/model/Transaction.java
+++ src/main/java/com/smartestgift/service/model/Transaction.java
@@ -1,11 +1,27 @@
 package com.smartestgift.service.model;
 
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.UUID;
+
 /**
  * Created with IntelliJ IDEA.
  * User: dikkini
  * Date: 10/6/13
  * Time: 2:07 PM
- * To change this template use File | Settings | File Templates.
  */
+@Entity
+@Table(name = "transaction")
 public class Transaction {
+
+    @Id
+    @Type(type = "pg-uuid")
+    private UUID uuid;
+
+    @ManyToOne
+    private Client client;
+
+    @ManyToOne
+    private Gift gift;
 }
