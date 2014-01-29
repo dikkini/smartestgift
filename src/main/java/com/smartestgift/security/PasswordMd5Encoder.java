f193dff516241e1653a94946a475b3fe0f191ee3
dikkini <dikkini@gmail.com>
2014-01-29 23:19:30 +0400

    password encoder and test

---
+++ src/main/java/com/smartestgift/security/PasswordMd5Encoder.java
@@ -0,0 +1,35 @@
+package com.smartestgift.security;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by dikkini on 29.01.14.
+ */
+public class PasswordMd5Encoder implements PasswordEncoder {
+    @Override
+    public String encode(CharSequence charSequence) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+            messageDigest.update(charSequence.toString().getBytes(), 0, charSequence.length());
+            String hashedPass = new BigInteger(1, messageDigest.digest()).toString(16);
+            if (hashedPass.length() < 32) {
+                hashedPass = "0" + hashedPass;
+                return hashedPass;
+            } else {
+                return null;
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public boolean matches(CharSequence charSequence, String s) {
+        return false;
+    }
+}
---
+++ src/main/test/com/smartestgift/security/PasswordMd5Test.java
@@ -0,0 +1,7 @@
+package com.smartestgift.security;
+
+/**
+ * Created by dikkini on 29.01.14.
+ */
+public class PasswordMd5Test {
+}
