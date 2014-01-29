aaf0fd5a5314a8a592e7373829d5d04cc7c63a85
dikkini <dikkini@gmail.com>
2014-01-29 23:26:08 +0400

    password encoder and test

--- src/main/test/com/smartestgift/security/PasswordMd5Test.java
+++ src/main/test/com/smartestgift/security/PasswordMd5Test.java
@@ -1,7 +1,18 @@
 package com.smartestgift.security;
 
+import org.junit.Test;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created by dikkini on 29.01.14.
  */
 public class PasswordMd5Test {
+    @Test
+    public void testPasswordMd5Encoder() {
+        PasswordEncoder encoder = new PasswordMd5Encoder();
+        String hashedPass = encoder.encode("koala");
+        assertEquals("a564de63c2d0da68cf47586ee05984d7", hashedPass);
+    }
 }
