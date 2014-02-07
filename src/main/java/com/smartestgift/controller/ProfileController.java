eeec37c010f48f509c0a6084f3be238c3b78cd05
dikkini <dikkini@gmail.com>
2014-02-08 03:10:39 +0400

    wired smartuserdao

--- src/main/java/com/smartestgift/controller/ProfileController.java
+++ src/main/java/com/smartestgift/controller/ProfileController.java
@@ -1,5 +1,8 @@
 package com.smartestgift.controller;
 
+import com.smartestgift.dao.SmartUserDAO;
+import com.smartestgift.dao.model.SmartUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +16,10 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class ProfileController {
+
+    @Autowired
+    SmartUserDAO smartUserDAO;
+
     @RequestMapping(value = "/profile", method = RequestMethod.GET)
     public ModelAndView signin() {
         ModelAndView mav = new ModelAndView("profile");
