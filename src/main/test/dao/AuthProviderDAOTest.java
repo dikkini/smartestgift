dc479ffe279b25ec31ef6283b1d70ed6e4341a99
dikkini <dikkini@gmail.com>
2014-02-20 18:42:41 +0400

    first tests

--- src/main/test/dao/AuthProviderDAOTest.java
+++ src/main/test/dao/AuthProviderDAOTest.java
@@ -1,8 +1,31 @@
 package dao;
 
+import com.smartestgift.dao.AuthProviderDAO;
+import com.smartestgift.dao.model.AuthProvider;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static junit.framework.Assert.assertTrue;
+
 /**
  * Created by dikkini on 20.02.14.
  * Email: dikkini@gmail.com
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:root-context.xml"})
 public class AuthProviderDAOTest {
+
+    @Autowired
+    AuthProviderDAO authProviderDAO;
+
+    @Test
+    public void testAuthProviderDAO() throws Exception {
+        AuthProvider facebookProvider = authProviderDAO.findFacebookProvider();
+        assertTrue(facebookProvider.getName().equals("facebook"));
+        AuthProvider applicationProvider = authProviderDAO.findApplicationProvider();
+        assertTrue(applicationProvider.getName().equals("application"));
+    }
 }
--- src/main/test/dao/RoleDAOTest.java
+++ src/main/test/dao/RoleDAOTest.java
@@ -1,8 +1,31 @@
 package dao;
 
+import com.smartestgift.dao.AuthProviderDAO;
+import com.smartestgift.dao.RoleDAO;
+import com.smartestgift.dao.model.AuthProvider;
+import com.smartestgift.dao.model.Role;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static junit.framework.Assert.assertTrue;
+
 /**
  * Created by dikkini on 20.02.14.
  * Email: dikkini@gmail.com
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:root-context.xml"})
 public class RoleDAOTest {
+    @Autowired
+    RoleDAO roleDAO;
+
+    @Test
+    public void testRoleDAO() throws Exception {
+        Role userRole = roleDAO.findUserRole();
+        assertTrue(userRole.getRole().equals("USER_ROLE"));
+
+    }
 }
