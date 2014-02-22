0e4e9cdf3de68609cccdcfc8f9763f07fa737b3d
dikkini <dikkini@gmail.com>
2014-02-22 23:41:55 +0400

    changing gift controller

--- src/main/java/com/smartestgift/controller/GiftController.java
+++ src/main/java/com/smartestgift/controller/GiftController.java
@@ -1,9 +1,15 @@
 package com.smartestgift.controller;
 
+import com.smartestgift.dao.GiftCategoryDAO;
+import com.smartestgift.dao.model.GiftCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * Created by dikkini on 14.02.14.
@@ -11,6 +17,9 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class GiftController {
+
+    @Autowired
+    GiftCategoryDAO giftCategoryDAO;
 
     @RequestMapping(value = "/gifts/mygifts", method = RequestMethod.GET)
     public ModelAndView myGifts() {
@@ -24,9 +33,18 @@ public class GiftController {
         return mav;
     }
 
-    @RequestMapping(value = "/gifts/gifts", method = RequestMethod.GET)
-    public ModelAndView gifts() {
-        ModelAndView mav = new ModelAndView("gifts/gifts");
+    @RequestMapping(value = "/gifts/categories/{giftCategoryId}", method = RequestMethod.GET)
+    public ModelAndView gifts(@PathVariable Integer giftCategoryId) {
+        List<GiftCategory> allGiftCategories = giftCategoryDAO.findAll();
+        ModelAndView mav = new ModelAndView("gifts/categories");
+        mav.addObject("allGiftCategories", allGiftCategories);
+
+        GiftCategory giftCategory = null;
+        if (giftCategoryId != 0) {
+            giftCategory = giftCategoryDAO.find(giftCategoryId);
+            mav.addObject("giftCategory", giftCategory);
+        }
+        mav.addObject("giftCategory", giftCategory);
         return mav;
     }
 
