3eed31ab3c134f765f1fefc3b04ec39c221894ac
dikkini <dikkini@gmail.com>
2014-03-05 10:38:48 +0400

    change method adding notifications, method for clearing request uri from all notifications

--- src/main/webapp/resources/main/js/utils.js
+++ src/main/webapp/resources/main/js/utils.js
@@ -9,29 +9,53 @@ $.updateQueryStringParameter = function(uri, key, value) {
 };
 
 $.updateNotifyBlockRequest = function(uri, successes, errors, information) {
+    uri = $.clearRequestFromNotifications(uri);
+
+    var errorsBlock = "errors=";
+    var successesBlock = "successes=";
+    var informationBlock = "information=";
+
     var separator = uri.indexOf('?') !== -1 ? "&" : "?";
     if (errors.length > 0) {
-        uri += separator + "errors=";
-        for (var error in errors) {
+        uri += separator + errorsBlock;
+        errors.forEach(function(error) {
             uri += error + ",";
-        }
+        });
     }
 
     separator = uri.indexOf('?') !== -1 ? "&" : "?";
     if (information.length > 0) {
-        uri += separator + "information=";
-        for (var info in information) {
+        uri += separator + informationBlock;
+        information.forEach(function(info) {
             uri += info + ",";
-        }
+        });
     }
 
     separator = uri.indexOf('?') !== -1 ? "&" : "?";
     if (successes.length > 0) {
-        uri += separator + "successes=";
-        for (var success in successes) {
+        uri += separator + successesBlock;
+        successes.forEach(function(success) {
             uri += success + ",";
-        }
+        });
     }
 
     return uri;
+};
+
+$.clearRequestFromNotifications = function(uri) {
+    var rtn = uri.split("?")[0],
+        param,
+        params_arr = [],
+        queryString = (uri.indexOf("?") !== -1) ? uri.split("?")[1] : "";
+    if (queryString !== "") {
+        params_arr = queryString.split("&");
+        for (var i = params_arr.length - 1; i >= 0; i -= 1) {
+            param = params_arr[i].split("=")[0];
+            if (param === "errors" || param === "information" || param === "successes") {
+                params_arr.splice(i, 1);
+            }
+        }
+        rtn = rtn + "?" + params_arr.join("&");
+    }
+    return rtn;
 };
